aab5bff9d713853b68225c1952095659f397f9d4
JordinDC7 <jordincamp23@outlook.com>
2025-12-17 18:04:15 -0700

    feat: scaffold clean architecture with EF Core and SQL Server

--- SaaSControlTower/src/Api/Program.cs
+++ SaaSControlTower/src/Api/Program.cs
@@ -1,3 +1,8 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        b => b.MigrationsAssembly("Infrastructure")
+    )
+);
+
+
+
+
 
 var app = builder.Build();
 
---
+++ SaaSControlTower/src/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Tenant> Tenants { get; set; }
+    }
+}
